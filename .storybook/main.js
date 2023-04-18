81ca393fe4d91c75ba1eed73a4cd79d765d5f0f2
Rizki Budi Prasetyo <rizkibudi513@gmail.com>
2023-04-18 23:09:39 +0700

    chore(storybook): upgrade storybook to v7.0.5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,42 +1,35 @@
 const path = require("path");
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    {
-      name: "@storybook/addon-postcss",
-      options: {
-        postcssLoaderOptions: {
-          implementation: require("postcss"),
-        },
-      },
-    },
-  ],
-  core: {
-    builder: "webpack5",
-  },
-  webpackFinal: (config) => {
-    const fileLoaderRule = config.module.rules.find(
-      (rule) => rule.test && rule.test.test(".svg"),
-    );
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", {
+    name: "@storybook/addon-postcss",
+    options: {
+      postcssLoaderOptions: {
+        implementation: require("postcss")
+      }
+    }
+  }, "@storybook/addon-mdx-gfm"],
+  webpackFinal: config => {
+    const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test(".svg"));
     fileLoaderRule.exclude = /\.svg$/;
-
     config.resolve.alias = {
       ...config.resolve?.alias,
-      "~": [path.resolve(__dirname, "../src/"), path.resolve(__dirname, "../")],
+      "~": [path.resolve(__dirname, "../src/"), path.resolve(__dirname, "../")]
     };
-
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
+      use: ["@svgr/webpack"]
     });
-
     config.resolve.fallback.util = require.resolve("util/");
-
     return config;
   },
+  framework: {
+    name: "@storybook/nextjs",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
